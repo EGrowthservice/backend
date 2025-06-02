4c9bef0df84cc980b9aa9823ddaccf9cb14fb5fe
hi204 <126770281+EGrowthservice@users.noreply.github.com>
2025-06-02 14:38:43 +0700

    Delete dist directory

--- dist/routes/utilsRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get('/health', (req, res) => {
-    res.status(200).json({ status: 'healthy' });
-});
-exports.default = router;
