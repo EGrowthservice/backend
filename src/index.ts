557f4b54bd77f7dbec27384610861dd7ee350c61
hi2004z <hieucv204@gmail.com>
2025-06-02 14:49:13 +0700

    upload

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+const port = parseInt(process.env.PORT || '3000', 10);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on port ${port}`);
 });
